e94abc10642c1c3cc3c01b6ea7f710f848cdaeae
MONNET CEDRINE <monnetcedrine@team-lab.com>
2022-09-20 11:50:37 +0900

    Saved

--- src/main/kotlin/com/example/todo/controller/TodoController.kt
+++ src/main/kotlin/com/example/todo/controller/TodoController.kt
@@ -1,18 +1,25 @@
 package com.example.todo.controller
 
+import com.example.todo.entity.Todo
 import com.example.todo.form.TodoForm
 import com.example.todo.service.TodoService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import java.time.LocalDate
 
 @Controller
 class TodoController(private val todoService: TodoService) {
 
   @GetMapping("/")
   fun index(model: Model): String {
-    model.addAttribute("todos", todoService.findAll())
+    val allTodos: List<Todo> = todoService.findAll()
+    model.addAttribute("todos", allTodos)
+    model.addAttribute("localDate", LocalDate.now())
+    for (todo: Todo in allTodos) {
+      println(todo.deadline!! < LocalDate.now())
+    }
     return "index"
   }
 
