c7fb250f194d077e5a2713760930b29260a2e44a
MONNET CEDRINE <monnetcedrine@team-lab.com>
2022-10-07 18:13:39 +0900

    bug fix

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -5,6 +5,5 @@ create table if not exists `todo` (
   `status` tinyint(1) not null default '0' comment 'ステータス',
   `create_time` datetime not null default current_timestamp comment '作成日時',
   `update_time` datetime not null default current_timestamp on update current_timestamp comment '更新日時',
-  primary key (`id`),
-  unique key `title_unique` (`title`)
+  primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='todoテーブル';
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import Checkbox from './Checkbox.jsx'
 
 export default function CreateTodoList () {
   const [todos, setTodos] = useState([]);
